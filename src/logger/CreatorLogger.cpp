226b06b1ef46583a5cdd3fad79d1f97dbf536cef
ceph1 <ceph1@ceph1>
2020-04-24 11:33:42 +0430

    Logger

---
+++ src/logger/CreatorLogger.cpp
@@ -0,0 +1,13 @@
+/*
+ * CreatorLogger.cpp
+ *
+ *  Created on: Apr 20, 2020
+ *      Author: ceph1
+ */
+
+#include "CreatorLogger.h"
+
+CreatorLogger::CreatorLogger() {
+
+}
+
---
+++ src/logger/FileLogger.h
@@ -0,0 +1,15 @@
+/*
+ * FileLogger.h
+ *
+ *  Created on: Apr 21, 2020
+ *      Author: ceph1
+ */
+
+#ifndef SRC_LOGGER_FILELOGGER_H_
+#define SRC_LOGGER_FILELOGGER_H_
+
+
+
+
+
+#endif /* SRC_LOGGER_FILELOGGER_H_ */
